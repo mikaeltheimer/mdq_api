655697e290144785ec6c7c2fd1962fa5029ca3d5
Stephen Young <me@hownowstephen.com>
2014-03-29 00:19:38 -0400

    no suit

--- mdq/settings.py
+++ mdq/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = (
     #'rest_framework.authtoken', # enable when setting up token-based authentication
 
     # Admin plugins
-    'suit',
+    #'suit',
     'django_extensions',
     'south',
 
